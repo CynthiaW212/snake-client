31982206a116ddd3515ff1fd92c27557e5b63a86
Yuze Wang <yuzewang621@gmail.com>
2023-05-07 12:34:26 -0600

    add client name to server

--- client.js
+++ client.js
@@ -8,6 +8,11 @@ const connect = function() {
   });
   // interpret incoming data as text
   conn.setEncoding("utf8");
+  
+  conn.on('connect', ()=> {
+    console.log("You have successfully connected to game server!");
+    conn.write("Name: CYN");
+  });
   conn.on("data", (data) => {
     // code that does something when the connection is first established
     console.log(data);
