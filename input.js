62b11d1e2561df18b261b05d76be001553cd61fd
Yuze Wang <yuzewang621@gmail.com>
2023-05-07 13:22:34 -0600

    add connection

--- input.js
+++ input.js
@@ -1,6 +1,10 @@
 // setup interface to handle user input from stdin
 
-const setupInput = function() {
+// Stores the active TCP connection object.
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -16,9 +20,18 @@ const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
   }
-  // if (key === 'e') {
-  //   console.log('the key is : ', key);
-  // }
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
+  if (key === 's') {
+    connection.write("Move: down");
+  }
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+  if (key === 'd') {
+    connection.write("Move: right");
+  }
 
 };
 
