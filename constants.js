d6e26762715ea44bc458f6d5925e5d0ad2a461d5
Yuze Wang <yuzewang621@gmail.com>
2023-05-07 16:10:55 -0600

    store constant in this file

---
+++ constants.js
@@ -0,0 +1,14 @@
+//constants.js
+const IP = '165.227.47.243';
+const PORT = '50541';
+const PLAYER_NAME = 'CYN';
+
+const KEY_MATCHING = {
+  'w': "Move: up",
+  'a': "Move: left",
+  'd': "Move: right",
+  's': "Move: down",
+  'x': "Say: I am win!"
+};
+
+module.exports = {IP, PORT,PLAYER_NAME, KEY_MATCHING};
