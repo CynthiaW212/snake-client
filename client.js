dbf244ac19ba0e6b3f11404f61dea4e57dc13532
Yuze Wang <yuzewang621@gmail.com>
2023-05-07 12:49:33 -0600

    practice up and left move(commented)

--- client.js
+++ client.js
@@ -12,6 +12,19 @@ const connect = function() {
   conn.on('connect', ()=> {
     console.log("You have successfully connected to game server!");
     conn.write("Name: CYN");
+
+    // conn.write('Move: up');//---Just for practice----
+    // setTimeout(()=>{
+    //   conn.write('Move: up');
+    // },500);
+    // setInterval(()=>{
+    //   conn.write('Move: left');
+    // },500);
+    // conn.write('Move: up');
+    // conn.write('Move: left');
+    // conn.write('Move: down');
+    // conn.write('Move: right');
+
   });
   conn.on("data", (data) => {
     // code that does something when the connection is first established
